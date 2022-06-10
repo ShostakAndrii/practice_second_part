084cea2fb5137ae83b3265a44dfca301a33f405f
ShostakAndrii <shostakandriii@gmail.com>
2022-06-10 11:59:17 +0300

    Added gif animation, added cache API

--- sw.js
+++ sw.js
@@ -1,5 +1,14 @@
 self.addEventListener("install", function(event) {
     console.log("[Service Worker] Installing Service Worker ...", event);
+    event.waitUntil(
+        caches.open("static")
+            .then(function(cache) {
+                console.log("precaching");
+                cache.add("/index.html");
+                cache.add("/");
+                cache.add("style.css");
+            })
+    );
 });
 
 self.addEventListener("activate", function(event) {
@@ -8,6 +17,14 @@ self.addEventListener("activate", function(event) {
 });
 
 self.addEventListener("fetch", function(event) {
-    console.log("[Service Worker] Fetching Service something ...", event);
-    event.respondWith(fetch(event.request));
+    event.respondWith(
+        caches.match(event.request)
+            .then(function(response) {
+                if (response)
+                    return response;
+                else
+                    return fetch(event.request);
+            }
+        )
+    );
 });
